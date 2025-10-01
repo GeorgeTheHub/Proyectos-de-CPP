2029d84c49ed77eaf1ff0205a47ec112c508ca7a
GeorgeTheHub <zs25018121@estudiantes.uv.mx>
2025-10-01 09:53:00 -0600

    Mi primer commit

---
+++ practica-1/holamundo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hola amigos de coatza-coatza, buenos dias en la UV!" << endl;
+    cout << "Es hora de activarnos con nuestro gran aliado de aprendizaje, C++!";
+    return 0;
+}
---
+++ practica-1/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hola compañeros coatzacoalqueños, buenos días en la UV!" << endl;
+    cout << "Es hora de activarnos con nuestro gran compañero, C++!";
+    return 0;
+}
