8c965e13dec648854b551a1f463b4e79625dd15d
GeorgeTheHub <zs25018121@estudiantes.uv.mx>
2025-10-01 09:54:39 -0600

    El commit inicial

--- practica-1/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main () {
-    cout << "Hola compañeros coatzacoalqueños, buenos días en la UV!" << endl;
-    cout << "Es hora de activarnos con nuestro gran compañero, C++!";
-    return 0;
-}
