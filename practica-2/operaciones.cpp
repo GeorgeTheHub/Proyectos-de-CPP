6decbd90391449d07f1e9f419b66e852dbc38076
GeorgeTheHub <zs25018121@estudiantes.uv.mx>
2025-10-06 09:01:09 -0600

    Expansion del gitignore

--- practica-2/operaciones.cpp
+++ practica-2/operaciones.cpp
@@ -5,15 +5,44 @@
 using namespace std;
 
 int main () {
-    int ValorA, ValorB, Signo;
+    int ValorA, ValorB;
+    char Signo;
 
         cout << "Inserte el valor A, para su problema matematico." << endl;
             cin >> ValorA;
+                if (cin.fail()) {
+                cout << "Entrada negada. No es un numero entero." << endl;
+                return 1;
+                }
+                
         cout << "Ahora, ingrese el valor B." << endl;
             cin >> ValorB;
-        cout << "Signo de operacion? (+    -   *)" << endl;
+                if (cin.fail()) {
+                cout << "Entrada negada. No es un numero entero." << endl;
+                return 1;
+                }
+                
+        cout << "Signo de operacion? ( + | - | * )" << endl;
             cin >> Signo;
-        cout << "A: " << ValorA << "\tSigno: " << (char)Signo << "\tB: " << ValorB << endl;
+                
+        cout << "\n" << "Orden: A " << Signo << " B" << endl;
 
+        cout << "Operacion: " << ValorA << " " << Signo << " " << ValorB << " = ";
+        if (Signo == '+'){
+            cout << ValorA + ValorB << endl;
+            cout << "SUMA CONCRETADA!" << endl;
+        }
+        else if (Signo == '-'){
+            cout << ValorA - ValorB << endl;
+            cout << "RESTA CONCRETADA!" << endl;
+        }
+        else if (Signo == '*'){
+            cout << ValorA * ValorB << endl;  
+            cout << "MULTIPLICACION CONCRETADA!" << endl;
+        }
+        else {
+            cout << "Operador fuera de rango, intente de nuevo." << endl;
+        }
+        
     return 0;
 }
